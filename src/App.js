6f7a95e859bfb901f6d0458e893afe4fef302f45
abdelaziz darragi <abdelazizdarragi00@gmail.com>
2020-08-03 15:50:46 +0200

    jsxcommit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{border:"solid 1px black" ,maxWidth:"100vw" }} >
+          
+         <br/>
+          <img src="/imageInSrc.jpg "/> <br/> 
+
+             <img src="/imageInPublic.jpg"/>  
+
+  </div>  
+  <iframe src="https://www.youtube.com/watch?v=9GtB5G2xGTY" width="540" height="450"></iframe>
+  
+  );
+}
+
+export default App;
